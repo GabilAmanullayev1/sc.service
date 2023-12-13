50877eb97530821147a524906a9f575d50d41a10
qabil amanullayev <qabil.amanullayev@gmail.com>
2023-12-14 00:37:16 +0400

    task

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-let nav = document.querySelector('nav');
-let isScrolled = false;
+document.addEventListener("DOMContentLoaded", () => {
+    let nav = document.querySelector('nav');
+    let menuBtn = document.querySelector('.nav-menu');
+    let hamburger = document.querySelector('.hamburger'); // Fixed the typo here
+    let closeBtn = document.querySelector('#close-button');
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) {
-        nav.classList.add('scrolled');
-    } else if (window.scrollY <= 300) {
-        nav.classList.remove('scrolled');
-    }
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 300) {
+            nav.classList.add('scrolled');
+        } else if (window.scrollY <= 300) {
+            nav.classList.remove('scrolled');
+        }
+    });
+
+    menuBtn.addEventListener('click', function () {
+        hamburger.style.display = "block";
+        hamburger.classList.add('hamburger-enter');
+        setTimeout(() => {
+            hamburger.classList.add('hamburger-enter-active');
+        }, 0);
+    });
+
+    closeBtn.addEventListener('click', function () {
+        hamburger.classList.remove('hamburger-enter-active');
+        hamburger.classList.add('hamburger-exit');
+        setTimeout(() => {
+            hamburger.style.display = "none";
+            hamburger.classList.remove('hamburger-exit');
+        }, 300);
+    });
 });
